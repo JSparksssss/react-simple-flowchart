404902d87b43e41df3310de9f9c1cf3df5427f22
JSparksssss <37327269+JSparksssss@users.noreply.github.com>
2022-07-11 00:04:27 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { Flowdemo } from "./Flowdemo";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>react-simple-flowchart demo</h1>
+      <Flowdemo />
+    </div>
+  );
+}
